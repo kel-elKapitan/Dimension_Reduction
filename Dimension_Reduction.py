de3b5bdcfe63db5f4f87b08c19d1b3bef755c135
Kelly McGowan <kelly.c.mcgowan80@gmail.com>
2021-10-03 18:26:50 +0100

    Delete Dimension_Reduction.py

--- Dimension_Reduction.py
+++
@@ -1,4 +0,0 @@
-# This project dataset is taken from https://
-
-# This project takes a dataset with hundreds of variables to find the best approach to reducing 
-# the number of variables without losing integrity
