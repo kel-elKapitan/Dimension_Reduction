02655ceb189e3718b511c5df5300efb6e8b7ded3
kel-elKapitan <kelly.c.mcgowan80@gmail.com>
2021-10-07 13:39:01 +0100

    introduced iterator. fixed #1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# dataset taken from https://www.kaggle.com/ananaymital/us-used-cars-dataset
+
+
+import pandas as pd
+
+
+# choose one type of vehicle as a sample becausae the dataset is over 9GB
+
+
+
+iter_csv = pd.read_csv('used_cars_data.csv', iterator=True, chunksize=500)
+raw = pd.concat([chunk[chunk['make_name'] == 'Jeep'] for chunk in iter_csv])
+
+
+print(len(raw))
 # Driver code for the application
