029808729fdc903094575671fe422060007d376a
kel-elKapitan <kelly.c.mcgowan80@gmail.com>
2021-10-20 09:49:37 +0100

    plotted number pre and post 2000. removed variables with over 20% missing values. Began imputing or removing of records with missing data in back_legroom

--- main.py
+++ main.py
@@ -2,17 +2,167 @@
 
 
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # choose one type of vehicle as a sample becausae the dataset is over 9GB
 
 
+####### Uncomment to create samples from the dataset based on model_name
+'''
 # import a sample of he dataset, all vehicles with the value 'Jeep' in the make_name column of the population
 iter_csv = pd.read_csv('used_cars_data.csv', iterator=True, chunksize=500)
-raw = pd.concat([chunk[chunk['make_name'] == 'Jeep'] for chunk in iter_csv])
+raw_jeep = pd.concat([chunk[chunk['make_name'] == 'Jeep'] for chunk in iter_csv])
 
-print(raw.head())
-print(len(raw))
+print(raw_jeep.head())
+# print(len(raw_jeep()))
 
+# save the sample dataset into a csv for easy reference and as a checkpoint in the process
+
+raw_jeep.to_csv('jeep.csv')
+'''
 
 # Driver code for the application
+
+# Import the sample dataset into memory
+raw_jeep = pd.read_csv('jeep.csv')
+raw_jeep['pre_2000'] = raw_jeep['year'] < 2000
+
+
+# Add a boolean column that states before 2000 is True and after 2000 is False
+pre_2000 = list()
+for i in raw_jeep['year']:
+    
+
+    if i < 2000:
+        pre_2000.append(True)
+    else:
+        pre_2000.append(False)
+
+raw_jeep['pre_2000'] = pre_2000
+
+#pre_2000 = raw_jeep[raw_jeep['year'] < 2000]
+#post_2000 = raw_jeep[raw_jeep['year'] >= 2000]
+
+pd.set_option('display.max_columns', None)
+print(raw_jeep.head())
+
+
+
+# plot a distribution of the number of cars sold after 31-12-1999
+
+sns.countplot(data=raw_jeep[raw_jeep['year'] >= 2000], x="year")
+plt.title('Number of cars by year post 2000')
+plt.xlabel('Year of Production')
+plt.ylabel('# of cars')
+plt.xticks(rotation= 90)
+# plt.xlim(1990, 2021)
+plt.savefig('post_2000.pdf')
+plt.show()
+
+# plot a distribution of the number of cars sold on or before 31-12-1999
+sns.countplot(data=raw_jeep[raw_jeep['year'] < 2000], x="year")
+plt.title('Number of cars by year pre 2000')
+plt.xlabel('Year of Production')
+plt.ylabel('# of cars')
+plt.xticks(rotation= 90)
+# plt.xlim(1990, 2021)
+plt.savefig('pre_2000.pdf')
+plt.show()
+
+
+# Lets check the missing value percentages
+pd. set_option('display.max_rows', None)
+my_missing = raw_jeep.isnull().sum()/len(raw_jeep)*100
+
+print(my_missing)
+
+
+# remove all variables with more than 20% missing values or higher
+variables = raw_jeep.columns
+col_length = len(raw_jeep.columns)
+variable = []
+
+for i in range(0,col_length):
+    if my_missing[i] <= 20: 
+        variable.append(variables[i])
+
+print('The remaining columns for analysis are:')
+print(variable)
+
+# create new dataframe with the remaining variables
+jeep_refined = raw_jeep.copy(deep=False)
+jeep_refined = jeep_refined[variable]
+
+
+# Lets impute some values that can be found to be true
+# back_legroom (8378 records missing)
+empty = jeep_refined[jeep_refined['back_legroom'].isna()]
+print('empty values in variable back_legroom')
+print(len(empty))
+print(empty.head())
+
+# lets look at the percentages of missing data within this subset of data
+my_missing = empty.isnull().sum()/len(empty)*100
+print('percentage of missing variables using the back_legroom NaN records ')
+print(my_missing)
+
+# get a list of unique values in the column model_name as this column has 100% completeness
+# 
+
+
+# plot back_legroom to year on a scatter using model_name as hue
+
+sns.scatterplot(x='year', y='back_legroom', data=jeep_refined, hue='model_name', y_jitter= True, x_jitter=True)
+plt.title('legroom by year')
+plt.xlabel('year')
+plt.ylabel('back_legroom')
+plt.xticks(rotation= 90)
+sns.despine()
+plt.show()
+
+
+my_make_names = empty['model_name'].unique()
+
+print('list of unique values from the model_name column')
+print(my_make_names)
+
+#empty_back_legroom = jeep_refined.count(NaN)
+#print('Records with empty back legroom values')
+#print(empty_back_legroom)
+
+
+# body_type
+
+# city_fuel_economy
+
+# engine_cylinders
+
+# engine_displacement
+
+# engine_type
+
+# exterior_color
+
+
+
+
+
+# lets have a look to find variables with a low variance and we will decide if we can remove them safely
+
+
+
+
+
+# the variable price is our Y variable, lets remove it from our dataset into its own variable y
+y = jeep_refined['price']
+print('dropping the price variable')
+jeep_refined.drop(['price'], axis=1, inplace=True)
+
+# variable without the price(y) variable
+print('the columns without price variable')
+print(jeep_refined.columns)
+print('The dataset without the price variable')
+print(jeep_refined.head())
+
+
