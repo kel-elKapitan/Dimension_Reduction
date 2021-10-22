bb16f951d1a7705b684ae32a58c8dfa10e455f70
kel-elKapitan <kelly.c.mcgowan80@gmail.com>
2021-10-22 13:11:56 +0100

    Saved all output to the output folder, ready for presentation

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ plt.title('Number of cars by year post 2000')
 plt.xlabel('Year of Production')
 plt.ylabel('# of cars')
 plt.xticks(rotation= 90)
-plt.savefig('post_2000.pdf')
+plt.savefig('output/post_2000.pdf')
 plt.show()
 
 # plot a distribution of the number of cars sold on or before 31-12-1999
@@ -50,15 +50,15 @@ plt.title('Number of cars by year pre 2000')
 plt.xlabel('Year of Production')
 plt.ylabel('# of cars')
 plt.xticks(rotation= 90)
-plt.savefig('pre_2000.pdf')
+plt.savefig('output/pre_2000.pdf')
 plt.show()
 
 
 # Lets check the missing value percentages
 pd.set_option('display.max_rows', None)
-my_missing = raw_jeep.isnull().sum()/len(raw_jeep)*100
-
-print(my_missing)
+my_missing_1 = raw_jeep.isnull().sum()/len(raw_jeep)*100
+my_missing_1.to_csv('output/my_missing_percent_1.csv')
+print(my_missing_1)
 
 # remove all variables with more than 20% missing values or higher
 variables = raw_jeep.columns
@@ -67,7 +67,7 @@ variable = []
 
 
 for i in range(0,col_length):
-    if my_missing[i] <= 20: 
+    if my_missing_1[i] <= 20: 
         variable.append(variables[i])
 
 print('The remaining columns for analysis are:')
@@ -138,18 +138,6 @@ jeep_refined['power'] = pd.to_numeric(jeep_refined['power'], errors='coerce')
 jeep_refined['torque'] = pd.to_numeric(jeep_refined['torque'], errors='coerce')
 jeep_refined['wheelbase'] = pd.to_numeric(jeep_refined['wheelbase'], downcast="float", errors='coerce')
 
-
-print(jeep_refined['back_legroom'].head())
-print(jeep_refined['front_legroom'].head())
-print(jeep_refined['engine_cylinders'].head())
-print(jeep_refined['fuel_tank_volume'].head())
-print(jeep_refined['height'].head())
-print(jeep_refined['length'].head())
-print(jeep_refined['maximum_seating'].head())
-print(jeep_refined['power'].head())
-print(jeep_refined['torque'].head())
-print(jeep_refined['wheelbase'].head())
-print(jeep_refined['width'].head())
 
 
 
@@ -230,17 +218,21 @@ print(jeep_refined.columns)
 
 
 
+# Save jeep_refined into csv file
+jeep_refined.to_csv('output/jeep_refined.csv')
 
 # change jeep refined into all numbers
-
 jeep_numeric = jeep_refined[['back_legroom','city_fuel_economy','daysonmarket','dealer_zip','engine_displacement','franchise_dealer','front_legroom','highway_fuel_economy','horsepower','is_new','latitude','longitude','mileage','price','savings_amount','seller_rating','year','pre_2000']]
+
 # check pecentages of missing data in jeep_refined
+my_missing_2 = jeep_numeric.isnull().sum()/len(jeep_numeric)*100
+my_missing_2.to_csv('output/my_missing_percent_2.csv')
 
-my_missing = jeep_numeric.isnull().sum()/len(jeep_numeric)*100
 print('All numbers missing data percentages')
-print(my_missing)
+print(my_missing_2)
 
-# drop the
+
+
 
 
 '''
@@ -260,7 +252,12 @@ price                   0.0
 ################################################################################################
 
 # the variable price is our Y variable, lets remove it from our dataset into its own variable y
-#y = jeep_numeric['price']
+y = jeep_numeric['price']
+
+# Save the y variable in a csv file
+y.to_csv('output/y.csv')
+
+
 #print('dropping the price variable')
 #jeep_numeric.drop(['price'], axis=1, inplace=True)
 
@@ -270,6 +267,8 @@ print(jeep_numeric.columns)
 print('The dataset without the price variable')
 print(jeep_numeric.head())
 
+# Save jeep_numeric to csv file
+jeep_numeric.to_csv('output/jeep_numeric.csv')
 
 
 
@@ -279,6 +278,7 @@ print(jeep_numeric.head())
 
 
 sns.heatmap(jeep_numeric.corr(), annot=True)
+plt.savefig('output/correlation.pdf')
 plt.show()
 # Lets check the datatypes of the variables
 
@@ -302,6 +302,7 @@ pca=PCA()
 jeep_tf = pca.fit_transform(jeep_numeric)
 # plot the variance
 plt.plot(pca.explained_variance_ratio_)
+plt.savefig('output/PCA_var_ratio.jpg')
 plt.show()
 
 
@@ -317,8 +318,9 @@ for i in jeep_refined['model_name'].unique():
 
 # visualise pca output
     
-    sns.scatterplot(x=my_model['price'], y=my_pca[:,1], hue=my_model['year'])
+    sns.scatterplot(x=my_model['price'], y=my_pca[:,0], hue=my_model['year'])
     plt.title('PCA with only ' + i)
+    plt.savefig('output/' + i + 'PCA_year.jpg')
     plt.show()
 
 
